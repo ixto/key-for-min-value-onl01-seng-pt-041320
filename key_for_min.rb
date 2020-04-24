4298af05d5450aade2578409c6886891c6256c6a
Joey <joey.alzamora@gmail.com>
2020-04-24 23:33:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,15 +3,29 @@
 # hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 
+# def key_for_min_value(name_hash) ############ ie hard coded
+#   lowest_key = nil
+#   x = 1000
+#   name_hash.each do |key, value|
+#     if value < x
+#       x = value
+#       lowest_key = key
+#     puts "#{key} => #{value}"
+# end
+# end
+#     lowest_key
+# end
+
+
 def key_for_min_value(name_hash)
+  lowest_value = nil
   lowest_key = nil
   x = 1000
   name_hash.each do |key, value|
-    if value < x
-      x = value
+    if lowest_value ==nil || value < lowest_value 
+      lowest_value = value
       lowest_key = key
-     puts "#{key} => #{value}"
- end
+  end
  end
     lowest_key
 end
